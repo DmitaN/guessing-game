a1ced8e42838d435d8a702e721f5d1f7ab33ce1e
Aleksandr Dmitriev <dmitan@bk.ru>
2022-08-08 12:46:37 +0300

    feat: i done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.assumptions = []
+        this.min = 0
+        this.max = 0
+        this.middle = 0
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        this.middle = Math.round((this.max - this.min) / 2) + this.min
+        this.assumptions.push(this.middle)
+        return this.middle
     }
 
     lower() {
-
+        this.max = this.middle
     }
 
     greater() {
-
+        this.min = this.middle
     }
 }
 
